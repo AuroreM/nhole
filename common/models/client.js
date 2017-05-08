bff32de025a8058d90c7075e4891cbb4f761d84c
AuroreM <aurorem@theodo.fr>
2017-05-08 14:44:30 +0200

    Enable to post message when sending SMS in the API

--- common/models/client.js
+++ common/models/client.js
@@ -3,10 +3,10 @@ module.exports = function(Client) {
   var SmsGateway = require('smsgateway');
 
   var gateway = new SmsGateway('a.malherbes@gmail.com','TOTOFOOBAR123');
-  var sendSMS = function (client) {
+  var sendSMS = function (client, message) {
     gateway.send(
       client.number,
-      "Hello",
+      message,
       46773
     ).then(function(data){
       console.log('Message sent');
@@ -14,51 +14,66 @@ module.exports = function(Client) {
       console.log('Failed', message);
     });
   }
-  Client.sendSMSToMorningClient = function(cb) {
+  Client.sendSMSToMorningClient = function(message, cb) {
     Client.find({where: {morning: true}}, function(err, results) {
-      results.map(sendSMS);
+      results.map(function(result) {
+        sendSMS(result, message)
+      });
       cb(null, results);
     });
   };
   Client.remoteMethod(
     'sendSMSToMorningClient', {
-      http: {path: '/morning', verb: 'get'},
+      accepts: [{arg: 'message', type: 'string'}],
+      http: {path: '/morning', verb: 'post'},
       returns: {arg: 'OK', type: 'string'}
     }
   );
 
-  Client.sendSMSToLunchClient = function(cb) {
+  Client.sendSMSToLunchClient = function(message, cb) {
     Client.find({where: {lunch: true}}, function(err, results) {
+      results.map(function(result) {
+        sendSMS(result, message)
+      });
       cb(null, results);
     });
   };
   Client.remoteMethod(
     'sendSMSToLunchClient', {
-      http: {path: '/lunch', verb: 'get'},
+      accepts: [{arg: 'message', type: 'string'}],
+      http: {path: '/lunch', verb: 'post'},
       returns: {arg: 'OK', type: 'string'}
     }
   );
 
-  Client.sendSMSToAfternoonClient = function(cb) {
+  Client.sendSMSToAfternoonClient = function(message, cb) {
     Client.find({where: {afternoon: true}}, function(err, results) {
+      results.map(function(result) {
+        sendSMS(result, message)
+      });
       cb(null, results);
     });
   };
   Client.remoteMethod(
     'sendSMSToAfternoonClient', {
-      http: {path: '/afternoon', verb: 'get'},
+      accepts: [{arg: 'message', type: 'string'}],
+      http: {path: '/afternoon', verb: 'post'},
       returns: {arg: 'OK', type: 'string'}
     }
   );
 
-  Client.sendSMSToEveningClient = function(cb) {
+  Client.sendSMSToEveningClient = function(message, cb) {
     Client.find({where: {evening: true}}, function(err, results) {
+      results.map(function(result) {
+        sendSMS(result, message)
+      });
       cb(null, results);
     });
   };
   Client.remoteMethod(
     'sendSMSToEveningClient', {
-      http: {path: '/evening', verb: 'get'},
+      accepts: [{arg: 'message', type: 'string'}],
+      http: {path: '/evening', verb: 'post'},
       returns: {arg: 'OK', type: 'string'}
     }
   );
