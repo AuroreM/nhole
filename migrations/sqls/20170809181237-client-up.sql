36abdcc0cc33f925a7a06a899b8004fee55c7d2e
Aurore <aurorem@theodo.fr>
2017-08-09 20:18:50 +0200

    Add migration

---
+++ migrations/sqls/20170809181237-client-up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE client (
+    firstname varchar(40),
+    lastname varchar(40),
+    number varchar(40),
+    morning boolean,
+    lunch boolean,
+    afternoon boolean,
+    evening boolean,
+    id bigserial
+);
