caf47fc75744dc08bf22c93009fec48695341bec
Aurore <aurorem@theodo.fr>
2017-10-15 18:48:31 +0200

    Refacto renderField

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Field } from 'redux-form';
-import TextField from 'material-ui/TextField';
 import Button from '../../components/common/Button';
+import TextField from '../../components/common/TextField';
 
 const Body = styled.div`
   display: flex;
@@ -14,26 +14,11 @@ const Body = styled.div`
 `;
 
 class Login extends React.Component {
-  styles = {
-    underlineFocusStyle: {
-      borderColor: 'rgb(30,144,255)',
-    },
-  };
-
-  renderField = field => (
-    <TextField
-      {...field.input}
-      type={field.type}
-      underlineFocusStyle={this.styles.underlineFocusStyle}
-      hintText={field.displayedName}
-    />
-  );
-
   render() {
     return (
       <Body>
-        <Field name="email" displayedName="Email" component={this.renderField} type="email" />
-        <Field name="password" displayedName="Mot de passe" component={this.renderField} type="password" />
+        <Field name="email" displayedName="Email" component={TextField} type="email" />
+        <Field name="password" displayedName="Mot de passe" component={TextField} type="password" />
         <Button label="Login" onClick={this.props.handleSubmit} />
       </Body>
     );
--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Field } from 'redux-form';
-import TextField from 'material-ui/TextField';
+import TextField from '../../components/common/TextField';
 import Button from '../../components/common/Button';
 
 const Body = styled.div`
@@ -14,45 +14,22 @@ const Body = styled.div`
 `;
 
 class Signup extends React.Component {
-  styles = {
-    underlineFocusStyle: {
-      borderColor: 'rgb(30,144,255)',
-    },
-  };
-
-  renderField = field => {
-    console.log(field);
-    return (
-      <TextField
-        {...field.input}
-        type={field.type}
-        underlineFocusStyle={this.styles.underlineFocusStyle}
-        hintText={field.displayedName}
-      />
-    );
-  };
-
   render() {
     return (
       <Body>
-        <Field name="email" displayedName="Email" component={this.renderField} type="email" />
-        <Field name="password" displayedName="Mot de passe" component={this.renderField} type="password" />
-        <Field
-          name="smsGatewayEmail"
-          displayedName="Email de SMS Gateway Me"
-          component={this.renderField}
-          type="email"
-        />
+        <Field name="email" displayedName="Email" component={TextField} type="email" />
+        <Field name="password" displayedName="Mot de passe" component={TextField} type="password" />
+        <Field name="smsGatewayEmail" displayedName="Email de SMS Gateway Me" component={TextField} type="email" />
         <Field
           name="smsGatewayPassword"
           displayedName="Mot de passe de SMS Gateway Me"
-          component={this.renderField}
+          component={TextField}
           type="password"
         />
         <Field
           name="smsGatewayDeviceId"
           displayedName="Device ID de SMS Gateway Me"
-          component={this.renderField}
+          component={TextField}
           type="string"
         />
         <Button label="Signup" onClick={this.props.handleSubmit} />
